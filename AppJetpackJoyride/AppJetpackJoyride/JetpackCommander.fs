0fd1eb8489378aeefa3a5b2dabb947af89a51b88
Marta <martamartino86@gmail.com>
2013-05-14 09:40:27 +0200

    [JJ] Aggiunta rete con operatori fsharp.

--- AppJetpackJoyride/AppJetpackJoyride/JetpackCommander.fs
+++ AppJetpackJoyride/AppJetpackJoyride/JetpackCommander.fs
@@ -1,10 +1,11 @@
-﻿// Ulteriori informazioni su F# all'indirizzo http://fsharp.net
-// Per ulteriori informazioni, vedere il progetto 'Esercitazione su F#'.
+﻿/// File:   JetpackCommander.fs
+/// Author: Marta Martino
 module Program
     open System.Windows.Forms
     open LeapDriver
     open ClonableLeapFrame
     open GestIT
+    open GestIT.FSharp
 
     let mutable lastUp:TimeStamp = -1L
 
@@ -17,22 +18,31 @@ module Program
             let finger =
                 f.PointableList.Values
                 |> Seq.maxBy (fun y -> y.Length)
-            finger.Position.y >= 200.f
+            finger.Position.y >= 230.f
+
+    let movefingerGoUp_h (sender, e:SensorEventArgs<LeapFeatureTypes, LeapEventArgs>) =
+        SendKeys.SendWait("{UP}")
+
+    let movefingerStay_h (sender, e:SensorEventArgs<LeapFeatureTypes, LeapEventArgs>) =
+        lastUp <- e.Event.Frame.Timestamp
+        SendKeys.SendWait("{UP}")
 
     let s = new LeapDriver.LeapSensor()
-
-    let movefinger_goup = new GroundTerm<_,_>(LeapFeatureTypes.MoveFinger, movefingerup 2)
-    let movefinger_stay = new GroundTerm<_,_>(LeapFeatureTypes.MoveFinger, movefingerup 5)
-    
+#if CLASSICWAY
     let i1 = new Iter<_,_>(movefinger_goup)
     let c1 = new Choice<_,_>(i1, movefinger_stay)
     i1.Gesture.Add(fun (sender,e) -> SendKeys.SendWait("{UP}"))
     let netgioco1 = c1.ToGestureNet(s)
-
     let i2 = new Iter<_,_>(movefinger_stay)
     let c2 = new Choice<_,_>(i2, movefinger_goup)
     i2.Gesture.Add(fun (sender,e) -> lastUp <- e.Event.Frame.Timestamp
                                      SendKeys.SendWait("{UP}"))
     let netgioco2 = c2.ToGestureNet(s)
-
+#endif
+    let movefinger_goup = (new GroundTerm<_,_>(LeapFeatureTypes.MoveFinger, movefingerup 2)) |^ movefingerGoUp_h
+    let movefinger_stay = (new GroundTerm<_,_>(LeapFeatureTypes.MoveFinger, movefingerup 5)) |^ movefingerStay_h
+    let expr1 = (!* movefinger_goup) |?| movefinger_stay
+    expr1.ToGestureNet(s) |> ignore
+    let expr2 = (!* movefinger_stay) |?| movefinger_goup
+    expr2.ToGestureNet(s) |> ignore
     System.Console.ReadLine() |> ignore
